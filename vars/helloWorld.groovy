588e9d9bf53e62c646dec6b0ef2449843da88711
Raghav Bhardwaj <raghav691989@gmail.com>
2024-06-14 18:03:09 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,3 @@
 def call() {
-  sh 'echo'
+  sh 'echo checkout scmGit(branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[credentialsId: '4f04306a-4af7-493c-9635-da71f3e4547a', url: 'https://github.com/raghav-bhardwaj/python/']])'
 }
